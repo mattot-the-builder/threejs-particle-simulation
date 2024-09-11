9a8eee162ba7c34242bded322e8675c39fc64a47
Hafizuddin <mattot.builder@gmail.com>
2024-09-11 16:47:02 +0800

    feat : added axis grid helper
    to help visualize the 3d world grid

---
+++ src/libs/ThreeApp.ts
@@ -0,0 +1,80 @@
+import { Camera, DirectionalLight, Light, Object3D, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+
+class ThreeApp {
+    private canvas: HTMLElement;
+    private renderer!: WebGLRenderer;
+    private scene!: Scene;
+    private camera!: Camera;
+
+    private light!: Light;
+
+    private objects: Object3D[] = [];
+
+    constructor(canvas: HTMLElement) {
+        this.canvas = canvas;
+        this.init();
+
+        // Bind animate to the current instance
+        this.animate = this.animate.bind(this);
+
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.animate);
+    }
+
+    init() {
+        //Initialization
+        this.renderer = new WebGLRenderer({ antialias: true, canvas: this.canvas });
+        this.scene = new Scene();
+
+        // Camera
+        this.camera = new PerspectiveCamera(75, 2, 1, 100);
+        this.camera.position.set(5, 5, 5);
+        this.camera.lookAt(0, 0, 0);
+
+        // Light
+        this.light = new DirectionalLight(0xffffff, 1);
+        this.light.position.set(-1, 2, 4);
+        this.scene.add(this.light);
+    }
+
+    animate(time: number) {
+        time *= 0.001;  // convert time to seconds
+
+        if (this.resize.call(this)) {
+            const canvas = this.renderer.domElement;
+            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            this.camera.updateProjectionMatrix();
+        }
+
+        this.objects.forEach((object) => {
+            object.rotation.x = time;
+            object.rotation.y = time;
+        });
+
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.animate);
+    }
+
+    resize() {
+        const canvas = this.renderer.domElement;
+        const pixelRatio = window.devicePixelRatio;
+        const width = Math.floor(canvas.clientWidth * pixelRatio);
+        const height = Math.floor(canvas.clientHeight * pixelRatio);
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            this.renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    addToScene(object: Object3D) {
+        this.objects.push(object);
+        this.scene.add(object);
+    }
+
+    getScene() {
+        return this.scene;
+    }
+}
+
+export default ThreeApp;
--- src/main.ts
+++ src/main.ts
@@ -1,86 +1,33 @@
 import './style.css'
-import { BoxGeometry, Camera, DirectionalLight, Light, Mesh, MeshBasicMaterial, MeshPhongMaterial, Object3D, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 
-class ThreeApp {
-    private canvas: HTMLElement;
-    private renderer!: WebGLRenderer;
-    private scene!: Scene;
-    private camera!: Camera;
+import { BoxGeometry, GridHelper, Mesh, MeshPhongMaterial, Object3D } from 'three';
+import GUI from 'lil-gui';
 
-    private light!: Light;
+import ThreeApp from './libs/ThreeApp';
+import AxisGridHelper from './helpers/AxisGridHelper';
 
-    private cube: Object3D;
+function main() {
+    const canvas = document.querySelector('#scene')! as HTMLCanvasElement;
+    const app = new ThreeApp(canvas);
+    const gui = new GUI();
 
-    constructor(canvas: HTMLElement) {
-        this.canvas = canvas;
-        this.init();
+    let cube: Object3D;
 
-        this.cube = this.createCube();
-        this.scene.add(this.cube);
-
-        // Bind animate to the current instance
-        this.animate = this.animate.bind(this);
-
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(this.animate);
-    }
-
-    init() {
-        //Initialization
-        this.renderer = new WebGLRenderer({ antialias: true, canvas });
-        this.scene = new Scene();
-
-        // Camera
-        this.camera = new PerspectiveCamera(75, 2, 0.1, 5);
-        this.camera.position.z = 2;
-
-        // Light
-        this.light = new DirectionalLight(0xffffff, 1);
-        this.light.position.set(-1, 2, 4);
-        this.addToScene(this.light);
-    }
-
-    animate(time: number) {
-        time *= 0.001;  // convert time to seconds
-
-        if (this.resize.call(this)) {
-            const canvas = this.renderer.domElement;
-            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            this.camera.updateProjectionMatrix();
-        }
-
-        this.cube.rotation.x = time;
-        this.cube.rotation.y = time;
-
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(this.animate);
-    }
-
-    resize() {
-        const canvas = this.renderer.domElement;
-        const pixelRatio = window.devicePixelRatio;
-        const width = Math.floor(canvas.clientWidth * pixelRatio);
-        const height = Math.floor(canvas.clientHeight * pixelRatio);
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-            this.renderer.setSize(width, height, false);
-        }
-        return needResize;
-    }
-
-    addToScene(object: Object3D) {
-        this.scene.add(object);
-    }
-
-    createCube() {
-        // Objects
+    // Objects
+    {
         const boxGeometry = new BoxGeometry(1, 1, 1);
         const material = new MeshPhongMaterial({ color: 0x44aa88 });
-        const cube = new Mesh(boxGeometry, material);
-        return cube;
+        cube = new Mesh(boxGeometry, material);
+        app.addToScene(cube);
     }
+
+    function makeAxisGrid(node: Object3D, label: string, units: number = 10) {
+        const helper = new AxisGridHelper(node, units);
+        gui.add(helper, 'visible').name(label);
+    }
+
+    makeAxisGrid(app.getScene(), 'scene');
+    makeAxisGrid(cube, 'cube', 10);
 }
 
-const canvas = document.querySelector('#scene')! as HTMLCanvasElement;
-
-const app = new ThreeApp(canvas);
+main();
